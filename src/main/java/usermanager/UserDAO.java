af7d3466d7ba8b90d74e02857b810013055740d8
Linfeng Z <33096195+lz346@users.noreply.github.com>
2020-11-12 21:27:21 -0500

    Initial commit

---
+++ src/main/java/usermanager/UserDAO.java
@@ -0,0 +1,124 @@
+package usermanager;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class UserDAO {
+    private final String jdbcURL;
+    private final String jdbcUsername;
+    private final String jdbcPassword;
+    private Connection jdbcConnection;
+
+    public UserDAO(String jdbcURL, String jdbcUsername, String jdbcPassword) {
+        this.jdbcURL = jdbcURL;
+        this.jdbcUsername = jdbcUsername;
+        this.jdbcPassword = jdbcPassword;
+    }
+    protected void connect() throws SQLException {
+        if (jdbcConnection == null || jdbcConnection.isClosed()) {
+            try {
+                Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            } catch (ClassNotFoundException e) {
+                throw new SQLException(e);
+            }
+            jdbcConnection = DriverManager.getConnection(
+                    jdbcURL, jdbcUsername, jdbcPassword);
+        }
+    }
+
+    protected void disconnect() throws SQLException {
+        if (jdbcConnection != null && !jdbcConnection.isClosed()) {
+            jdbcConnection.close();
+        }
+    }
+
+    public void insertUser(User user) throws SQLException {
+        String sql = "INSERT INTO book (title, author, price) VALUES (?, ?, ?)";
+        connect();
+        try (PreparedStatement statement = jdbcConnection.prepareStatement(sql)) {
+            statement.setString(1, user.getName());
+            statement.setString(2, user.getEmail());
+            statement.setString(3, user.getCountry());
+            System.out.println(statement);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public User selectUser(int id) throws SQLException {
+        User user = null;
+        String sql = "select id,name,email,country from users where id =?";
+        connect();
+        try (PreparedStatement preparedStatement = jdbcConnection.prepareStatement(sql)) {
+            preparedStatement.setInt(1, id);
+            System.out.println(preparedStatement);
+
+            ResultSet rs = preparedStatement.executeQuery();
+
+            while (rs.next()) {
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+                String country = rs.getString("country");
+                user = new User(id, name, email, country);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return user;
+    }
+
+    public List < User > selectAllUsers() throws SQLException {
+
+        // using try-with-resources to avoid closing resources (boiler plate code)
+        List< User > users = new ArrayList< >();
+        connect();
+        String sql = "select * from users";
+        // Step 1: Establishing a Connection
+        try (PreparedStatement preparedStatement = jdbcConnection.prepareStatement(sql)) {
+            // Step 3: Execute the query or update query
+            ResultSet rs = preparedStatement.executeQuery();
+
+            // Step 4: Process the ResultSet object.
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+                String country = rs.getString("country");
+                users.add(new User(id, name, email, country));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return users;
+    }
+
+    public boolean deleteUser(int id) throws SQLException {
+        boolean rowDeleted;
+        connect();
+        String sql = "delete from users where id = ?;";
+        try (PreparedStatement statement = jdbcConnection.prepareStatement(sql)) {
+            statement.setInt(1, id);
+            rowDeleted = statement.executeUpdate() > 0;
+        }
+        return rowDeleted;
+    }
+
+    public boolean updateUser(User user) throws SQLException {
+        boolean rowUpdated;
+        connect();
+        String sql = "update users set name = ?,email= ?, country =? where id = ?;";
+        try (PreparedStatement statement = jdbcConnection.prepareStatement(sql)) {
+            statement.setString(1, user.getName());
+            statement.setString(2, user.getEmail());
+            statement.setString(3, user.getCountry());
+            statement.setInt(4, user.getId());
+            rowUpdated = statement.executeUpdate() > 0;
+        }
+        return rowUpdated;
+    }
+
+
+}
